5e7de83e91f4d86c0f9745474a9da9f4625c324d
naytun <naytunthein70@gmail.com>
2025-01-29 21:17:33 -0500

    remove tests

--- src/types/novel.d.ts
+++ src/types/novel.d.ts
@@ -1,13 +1,11 @@
-declare module 'novel' {
-	export interface Chapter {
-		volume: string
-		chapter: string
-		title?: string
-	}
+export interface Chapter {
+	volume: string
+	chapter: string
+	title?: string
+}
 
-	export interface ChapterNavigation {
-		current: Chapter
-		prev?: Chapter
-		next?: Chapter
-	}
+export interface ChapterNavigation {
+	current: Chapter
+	prev?: Chapter
+	next?: Chapter
 }
